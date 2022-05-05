cebbccaa555e189ccdbba42021d04440bf98bd82
JerromyC <59486979+JerromyC@users.noreply.github.com>
2022-05-04 22:17:42 -0500

    initial commit
    Adding files

---
+++ quotes.js
@@ -0,0 +1,34 @@
+let localQuotes = [
+    {
+        "text": "I became insane, with long intervals of horrible sanity.",
+        "author": "Edgar Allan Poe"
+    },
+    {
+        "text": "All that we see or seem is but a dream within a dream.",
+        "author": "Edgar Allan Poe"
+    },
+    {
+        "text": "We loved with a love that was more than love.",
+        "author": "Edgar Allan Poe"
+    },
+    {
+        "text": "I have great faith in fools - self-confidence my friends will call it.",
+        "author": "Edgar Allan Poe"
+    },
+    {
+        "text": "Those who dream by day are cognizant of many things which escape those who dream only by night.",
+        "author": "Edgar Allan Poe"
+    },
+    {
+        "text": "I was never really insane except upon occasions when my heart was touched.",
+        "author": "Edgar Allan Poe"
+    },
+    {
+        "text": "Sleep, those little slices of death — how I loathe them.",
+        "author": "Edgar Allan Poe"
+    },
+    {
+        "text": "Years of love have been forgot, In the hatred of a minute.",
+        "author": "Edgar Allan Poe"
+    },
+]
